6cc1a31f3a78ca25051a944292b4c34453454936
libaojin <1497274207@qq.com>
2025-06-23 23:44:26 +0800

    flask修改数据库返回数据不全，写了order前端

--- Day21/views/order.py
+++ Day21/views/order.py
@@ -7,13 +7,26 @@ od = Blueprint('order', __name__)
 @od.route('/order/list')
 def order_list():
     user_info = session.get("user_info")
-    role = user_info['role']
+    real_name = user_info['real_name']
+    print("user_info:", user_info)
+    role = int(user_info.get("role", 1))
     if role == 2:
-        data_list = fetch_all("select * from `order`")
+        sql = "select * from orders LEFT JOIN userinfo on orders.user_id = userinfo.id"
+        data_list = fetch_all(sql)
     else:
-        data_list = fetch_all("select * from `order` where user_id = %s",(user_info["id"],))
-    print(data_list)
-    return render_template('order_list.html', data_list=data_list)
+        sql = "select * from orders LEFT JOIN userinfo on orders.user_id = userinfo.id where orders.user_id = %s"
+        data_list = fetch_all(sql, (user_info["id"],))
+    status_dict = {
+        1: "待执行",
+        2: "正在执行",
+        3: "完成",
+        4: "失败",
+    }
+    print("执行SQL:", sql)
+    print("返回条数:", len(data_list))
+    for row in data_list:
+        print(row)
+    return render_template('order_list.html', data_list=data_list, status_dict=status_dict,real_name=real_name)
 
 
 @od.route('/order/create')
